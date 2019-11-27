b2d85a055d4933b2016d594a2a167af88092839a
maestro-game <super.belovvadim@yandex.ru>
2019-11-28 00:19:45 +0300

    Revert "remake enums"
    This reverts commit a5518b1a32a5f7b4e204217d287d9c8fc3941158.

--- src/Field.java
+++ src/Field.java
@@ -1,6 +1,5 @@
 import javafx.scene.Group;
 import javafx.scene.shape.Rectangle;
-import models.Figure;
 import models.Figures;
 
 public class Field {
@@ -10,22 +9,6 @@ public class Field {
 
         public Cell(int x, int y) {
             this.x = x;
-            this.y = y;
-        }
-
-        public int getX() {
-            return x;
-        }
-
-        public int getY() {
-            return y;
-        }
-
-        public void setX(int x) {
-            this.x = x;
-        }
-
-        public void setY(int y) {
             this.y = y;
         }
     }
